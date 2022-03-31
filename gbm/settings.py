34ece430cdfdd334e3e4660e354bb291b6626178
Juan Pablo <Money_Honey_Michi@outlook.es>
2022-03-31 14:19:57 -0600

    appCommit11

--- gbm/settings.py
+++ gbm/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 import os
+import django_heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
